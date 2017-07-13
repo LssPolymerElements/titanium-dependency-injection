a045c30e8fb02239a5077ebf6c9e876c9c9d7f41
anthony-thomas <anthony-thomas@leavitt.com>
2017-07-13 16:21:06 -0600

    Changes js?

--- titanium-requester-mixin.js
+++ titanium-requester-mixin.js
@@ -1,36 +1,24 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 let TitaniumRequesterMixin = (superClass) => {
     return class extends superClass {
-        requestProvider(key) {
-            return __awaiter(this, void 0, void 0, function* () {
-                let resolveFn = (value) => { return value; };
-                let promise = new Promise((resolve, reject) => {
-                    resolveFn = resolve;
-                });
-                let options = {
-                    detail: { key, resolve: resolveFn },
-                    bubbles: true,
-                    composed: true,
-                    cancelable: true
-                };
-                const event = new CustomEvent('titanium-request-instance', options);
-                window.dispatchEvent(event);
-                return promise;
+        async requestProvider(key) {
+            let resolveFn = (value) => { return value; };
+            let promise = new Promise((resolve, reject) => {
+                resolveFn = resolve;
             });
+            let options = {
+                detail: { key, resolve: resolveFn },
+                bubbles: true,
+                composed: true,
+                cancelable: true
+            };
+            const event = new CustomEvent('titanium-request-instance', options);
+            window.dispatchEvent(event);
+            return promise;
         }
-        requestInstance(key) {
-            return __awaiter(this, void 0, void 0, function* () {
-                let value = yield this.requestProvider(key);
-                console.log('request instance value:', value);
-                return value;
-            });
+        async requestInstance(key) {
+            let value = await this.requestProvider(key);
+            console.log('request instance value:', value);
+            return value;
         }
         value(key) {
             return () => this.requestInstance(key);
