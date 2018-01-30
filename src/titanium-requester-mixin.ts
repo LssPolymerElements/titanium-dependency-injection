15275271d264a6524b095a32db44ac75d0c72a4b
Aaron Drabeck <github@aarondrabeck.com>
2018-01-30 14:15:28 -0700

    Moves mixins to src folder; fixes build issues.

--- src/titanium-requester-mixin.ts
+++ src/titanium-requester-mixin.ts
@@ -1,11 +1,11 @@
 ﻿let TitaniumRequesterMixin = function<T extends new (...args: any[]) => {}>(superClass: T) {
   return class extends superClass {
     declare dispatchEvent: any;
-    private async requestProvider(key: string): Promise<any> {
+    async requestProvider(key: string): Promise<any> {
       let resolveFn = (value: any) => {
         return value;
       };
-      let promise = new Promise<any>((resolve, reject) => {
+      let promise = new Promise<any>((resolve) => {
         resolveFn = resolve;
       });
       let options = {detail: {key, resolve: resolveFn}, bubbles: true, composed: true, cancelable: true};
@@ -19,7 +19,7 @@
       return value;
     }
 
-    private value(key: string) {
+    value(key: string) {
       return () => this.requestInstance(key);
     }
   };
